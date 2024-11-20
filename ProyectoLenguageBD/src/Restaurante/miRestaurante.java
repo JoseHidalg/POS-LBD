1ffc0dec6fbae8511b90700ebd235a92d4cfb39b
jhidalgo-ufide <148737164+jhidalgo-ufide@users.noreply.github.com>
2024-11-19 21:37:38 -0600

    cambio

--- ProyectoLenguageBD/src/Restaurante/miRestaurante.java
+++ ProyectoLenguageBD/src/Restaurante/miRestaurante.java
@@ -5,7 +5,7 @@ public class miRestaurante {
 
     public static void main(String[] args) {
     
-    	    
+    	   //cambios 
     	        javax.swing.SwingUtilities.invokeLater(() -> {
     	            ventanaLogin ventanaLogin = new ventanaLogin();
     	            ventanaLogin.setVisible(true);
